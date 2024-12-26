7700fae0ce985493dbe86d5ba187b930744707e6
Oliver Epper <oliver.epper@gmail.com>
2024-12-27 00:51:44 +0100

    Added a FIXME about an open question

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -181,7 +181,9 @@ TO can be \\='light or \\='dark"
   :hook
   (emacs-startup . oe/startup-message)
   (kill-emacs . oe/kill-unsafe-buffers)
-  (dired-mode . dired-hide-details-mode)
+  (dired-mode . dired-hide-details-mode) ; FIXME: Could this be done
+					 ; in a use-package dired
+					 ; block?
 	      
   :bind
   (("s-." . (lambda () (interactive) (find-file user-init-file)))
